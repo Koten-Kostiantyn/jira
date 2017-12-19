32ad1d801689e543cdc24e3a2af9f9166c47b0c7
Kostiantyn <oookotooo@users.noreply.github.com>
2017-12-19 09:26:38 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,3 +9,11 @@ echo "Finish mySQL installation"
 echo "Starting JIRA installation"
 ansible-playbook jira.yml
 echo "Finish JIRA installation"
+echo "Starting JIRA"
+systemctl start jira 
+sleep 5
+systemctl status jira
+echo "Open 8080 port in firewalld"
+firewall-cmd --zone=public --add-port=8080/tcp --permanent
+firewall-cmd --reload
+echo "Done"
